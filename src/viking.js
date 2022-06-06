72af32cb714c8a6b8968e805d38a32e795388789
gomachuca <104030177+gomachuca@users.noreply.github.com>
2022-06-06 20:10:12 +0200

    Update viking.js

--- src/viking.js
+++ src/viking.js
@@ -27,13 +27,54 @@ class Viking extends Soldier {
   }
 
   battleCry() {
-    return 'Odin Owns You All';
+    return 'Odin Owns You All!';
   }
 }
 
 //
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    if (this.health > 0) {
+      return `A Saxon has received ${theDamage} points of damage`;
+    }
+    return `A Saxon has died in combat`;
+  }
+
+  attack() {}
+}
+// ### Iteration 3: Saxon
+
+// A `Saxon` is a weaker kind of `Soldier`. Unlike a `Viking`, a `Saxon` has no name. Their `receiveDamage()` method will also be different than the original `Soldier` version.
+
+// Modify the `Saxon`, constructor function, have it inherit from `Soldier` and re-implement the `receiveDamage()` method for `Saxon`.
+
+// #### inheritance
+
+// - `Saxon` should extend `Soldier`
+
+// #### class
+
+// - You don't have to include constructor method since this class will inherit perfectly from the parents class, both, the health and the strength (it `extends` Soldier class :wink: )
+
+// #### `attack()` method
+
+// This method should be **inherited** from `Soldier`, no need to re-implement it.
+
+// - should be a function
+// - should receive **0 arguments**
+// - should return **the `strength` property of the `Saxon`**
+
+// #### `receiveDamage()` method
+
+// This method needs to be **re-implemented** for `Saxon` because the `Saxon` version needs to have different return values.
+
+// - should be a function
+// - should receive **1 argument** (the damage)
+// - should remove the received damage from the `health` property
+// - **if the Saxon is still alive**, it should return **_"A Saxon has received DAMAGE points of damage"_**
+// - **if the Saxon dies**, it should return **_"A Saxon has died in combat"_**
 
 // War
 class War {}
