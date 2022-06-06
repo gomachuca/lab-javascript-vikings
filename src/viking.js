3a04633b5b0b6d47d3a9086dc0d9dae136d7ae15
gomachuca <104030177+gomachuca@users.noreply.github.com>
2022-06-06 17:28:16 +0200

    vikings comit hasta viking

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,24 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+  constructor(health, strenght) {
+    this.health = health;
+    this.strength = strenght;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {
+   this.health = this.health - theDamage; //should remove the received damage from the health property
+  }
+}
 // Viking
 class Viking {}
-
+ñjskjsd
 // Saxon
 class Saxon {}
 
 // War
 class War {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
